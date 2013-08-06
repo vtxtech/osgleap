5da6cf8b901e54aa11378f74dad974b8753161b8
vtxtech@googlemail.com <vtxtech@googlemail.com@5dc6e85c-af96-51f9-56c2-3147ef9b22fe>
2013-08-06 06:39:16 +0000

    Tagged vTagged v0.1.2

---
+++ tags/osgLeap-0.1.2/src/osgLeap/Controller.cpp
@@ -0,0 +1,40 @@
+/*
+ * Library osgLeap
+ * Copyright (C) 2013 Johannes Scholz/vtxtech. All rights reserved.
+ *
+ * This file is licensed under the GNU Lesser General Public License 3 (LGPLv3),
+ * but distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#include <osgLeap/Controller>
+
+//-- OSG: osg --//
+#include <osg/Notify>
+
+#ifdef LEAPSDK_080_COMPATIBILITYMODE
+// Fix for missing implementation of virtual destructor
+namespace Leap {
+	Controller::~Controller() {
+
+	}
+}
+#endif
+
+namespace osgLeap {
+
+	Controller* Controller::instance(bool erase)
+	{
+		static osg::ref_ptr<Controller> instance_ = new Controller;
+
+		OSG_DEBUG<<"Accessed: osgLeap::Controller="<<instance_<<std::endl;
+
+		if (erase) 
+		{   
+			instance_ = NULL;
+		}
+		return instance_.get(); // will return NULL on erase
+	}
+
+} // namespace osgLeap
