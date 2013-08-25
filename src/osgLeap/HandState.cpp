6cea2c3b6965a46e622662ec6a3009f8e4003cee
vtxtech <vtxtech@googlemail.com>
2013-08-25 18:30:15 +0000

    Removed left hand images. Inversed TexCoords to mirror instead.

--- src/osgLeap/HandState.cpp
+++ src/osgLeap/HandState.cpp
@@ -31,45 +31,9 @@ namespace osgLeap {
 		}
 	};
 
-	HandState::HandState(): osg::Geode(), Leap::Listener(),
-		frame_(Leap::Frame())
+	void HandState::createHandQuad(WhichHand hand)
 	{
-		addUpdateCallback(new UpdateCallback());
-
-		osgLeap::Controller::instance()->addListener(*this);
-		texLeftHand_.push_back(osgDB::readImageFile("nolefthand.png"));
-		texLeftHand_.push_back(osgDB::readImageFile("lefthand0s.png"));
-		texLeftHand_.push_back(osgDB::readImageFile("lefthand1s.png"));
-		texLeftHand_.push_back(osgDB::readImageFile("lefthand2s.png"));
-		texLeftHand_.push_back(osgDB::readImageFile("lefthand3s.png"));
-		texLeftHand_.push_back(osgDB::readImageFile("lefthand4s.png"));
-		texLeftHand_.push_back(osgDB::readImageFile("lefthand5s.png"));
-
-		texRightHand_.push_back(osgDB::readImageFile("norighthand.png"));
-		texRightHand_.push_back(osgDB::readImageFile("righthand0s.png"));
-		texRightHand_.push_back(osgDB::readImageFile("righthand1s.png"));
-		texRightHand_.push_back(osgDB::readImageFile("righthand2s.png"));
-		texRightHand_.push_back(osgDB::readImageFile("righthand3s.png"));
-		texRightHand_.push_back(osgDB::readImageFile("righthand4s.png"));
-		texRightHand_.push_back(osgDB::readImageFile("righthand5s.png"));
-
-		texLeftHand_.at(0)->scaleImage(1024, 1024, 1);
-		texLeftHand_.at(1)->scaleImage(1024, 1024, 1);
-		texLeftHand_.at(2)->scaleImage(1024, 1024, 1);
-		texLeftHand_.at(3)->scaleImage(1024, 1024, 1);
-		texLeftHand_.at(4)->scaleImage(1024, 1024, 1);
-		texLeftHand_.at(5)->scaleImage(1024, 1024, 1);
-		texLeftHand_.at(6)->scaleImage(1024, 1024, 1);
-
-		texRightHand_.at(0)->scaleImage(1024, 1024, 1);
-		texRightHand_.at(1)->scaleImage(1024, 1024, 1);
-		texRightHand_.at(2)->scaleImage(1024, 1024, 1);
-		texRightHand_.at(3)->scaleImage(1024, 1024, 1);
-		texRightHand_.at(4)->scaleImage(1024, 1024, 1);
-		texRightHand_.at(5)->scaleImage(1024, 1024, 1);
-		texRightHand_.at(6)->scaleImage(1024, 1024, 1);
-
-		{
+		if (hand == LEFT_HAND) {
 			osg::ref_ptr<osg::Geometry> geom = new osg::Geometry();
 			osg::ref_ptr<osg::Vec3Array> va = new osg::Vec3Array();
 			va->push_back(osg::Vec3(0.0f, 0.0f, 0.0f));
@@ -89,10 +53,10 @@ namespace osgLeap {
 
 			osg::Vec2 myTexCoords[] =
 			{
-				osg::Vec2(0,0),
 				osg::Vec2(1,0),
-				osg::Vec2(1,1),
-				osg::Vec2(0,1)
+				osg::Vec2(0,0),
+				osg::Vec2(0,1),
+				osg::Vec2(1,1)
 			};
 
 			int numTexCoords = sizeof(myTexCoords)/sizeof(osg::Vec2);
@@ -103,14 +67,12 @@ namespace osgLeap {
 
 			lhTex_ = new osg::Texture2D();
 			lhTex_->setDataVariance(osg::Object::DYNAMIC);
-			lhTex_->setImage(texLeftHand_.at(0));
+			lhTex_->setImage(texRightHand_.at(0));
 			geom->getOrCreateStateSet()->setTextureAttributeAndModes(0, lhTex_, osg::StateAttribute::ON);
 			geom->getOrCreateStateSet()->setMode(GL_BLEND, osg::StateAttribute::ON);
 
 			addDrawable(geom);
-		}
-
-		{
+		} else {
 			osg::ref_ptr<osg::Geometry> geom = new osg::Geometry();
 			osg::ref_ptr<osg::Vec3Array> va = new osg::Vec3Array();
 			va->push_back(osg::Vec3(128.0f, 0.0f, 0.0f));
@@ -150,6 +112,33 @@ namespace osgLeap {
 
 			addDrawable(geom);
 		}
+
+	}
+
+	HandState::HandState(): osg::Geode(), Leap::Listener(),
+		frame_(Leap::Frame())
+	{
+		addUpdateCallback(new UpdateCallback());
+
+		osgLeap::Controller::instance()->addListener(*this);
+		texRightHand_.push_back(osgDB::readImageFile("norighthand.png"));
+		texRightHand_.push_back(osgDB::readImageFile("righthand0s.png"));
+		texRightHand_.push_back(osgDB::readImageFile("righthand1s.png"));
+		texRightHand_.push_back(osgDB::readImageFile("righthand2s.png"));
+		texRightHand_.push_back(osgDB::readImageFile("righthand3s.png"));
+		texRightHand_.push_back(osgDB::readImageFile("righthand4s.png"));
+		texRightHand_.push_back(osgDB::readImageFile("righthand5s.png"));
+
+		texRightHand_.at(0)->scaleImage(1024, 1024, 1);
+		texRightHand_.at(1)->scaleImage(1024, 1024, 1);
+		texRightHand_.at(2)->scaleImage(1024, 1024, 1);
+		texRightHand_.at(3)->scaleImage(1024, 1024, 1);
+		texRightHand_.at(4)->scaleImage(1024, 1024, 1);
+		texRightHand_.at(5)->scaleImage(1024, 1024, 1);
+		texRightHand_.at(6)->scaleImage(1024, 1024, 1);
+
+		createHandQuad(LEFT_HAND);
+		createHandQuad(RIGHT_HAND);
 	}
 
 	HandState::~HandState()
@@ -174,7 +163,7 @@ namespace osgLeap {
 	{
 		Leap::Frame frame = frame_;
 
-		osg::Image* lh = texLeftHand_.at(0);
+		osg::Image* lh = texRightHand_.at(0);
 		osg::Image* rh = texRightHand_.at(0);
 
 		if (frame.hands().count() > 0) {
@@ -184,7 +173,7 @@ namespace osgLeap {
 				rh = texRightHand_.at(right.fingers().count()+1);
 			} else {
 				rh = texRightHand_.at(right.fingers().count()+1);
-				lh = texLeftHand_.at(left.fingers().count()+1);
+				lh = texRightHand_.at(left.fingers().count()+1);
 			}
 		}
 
