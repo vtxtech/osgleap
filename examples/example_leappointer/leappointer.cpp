11e1174c3d73bcaa60e24338b5c99871ec6d48dd
vtxtech <vtxtech@googlemail.com>
2013-09-25 15:47:22 +0000

    Code cleanup, Removal of copy paste code

--- examples/example_leappointer/leappointer.cpp
+++ examples/example_leappointer/leappointer.cpp
@@ -1,6 +1,6 @@
 /*
 * Example leappointer
-* Copyright (C) 2013 Johannes Scholz/vtxtech. All rights reserved.
+* Copyright (C) 2013 Johannes Kroeger/vtxtech. All rights reserved.
 *
 * This file is licensed under the GNU Lesser General Public License 3 (LGPLv3),
 * but distributed WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -211,7 +211,7 @@ int main(int argc, char** argv)
 
     viewer.addEventHandler(new osgViewer::WindowSizeHandler);
 
-    // ToDo/j.scholz: Remove this...
+    // ToDo/j.kroeger: Remove this...
     viewer.setThreadingModel(osgViewer::ViewerBase::SingleThreaded);
 
     // Setup example osgWidgets
