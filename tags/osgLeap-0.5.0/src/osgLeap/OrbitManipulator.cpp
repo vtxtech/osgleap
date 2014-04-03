fa9781f7a2af356805eececc0aae267d3361b42b
vtxtech@googlemail.com <vtxtech@googlemail.com@5dc6e85c-af96-51f9-56c2-3147ef9b22fe>
2014-04-03 05:44:28 +0000

    Tagged v.0.5.0

---
+++ tags/osgLeap-0.5.0/src/osgLeap/OrbitManipulator.cpp
@@ -0,0 +1,304 @@
+/*
+* Library osgLeap
+* Copyright (C) 2013 Johannes Kroeger/vtxtech. All rights reserved.
+*
+* This file is licensed under the GNU Lesser General Public License 3 (LGPLv3),
+* but distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+*/
+
+#include <osgLeap/OrbitManipulator>
+
+//-- Project --//
+#include <osgLeap/Event>
+
+//-- OSG: osg --//
+#include <osg/Referenced>
+#include <osg/Timer>
+
+//-- Leap --//
+#include <LeapMath.h>
+
+namespace osgLeap {
+
+    // Workaround for LeapSDK 0.8.0 or lower which do not include stabilizedPalmPosition
+    // ToDo/j.kroeger: Detect version of LeapSDK instead
+    Leap::Vector getPalmPosition(const Leap::Hand& hand) {
+#ifdef LEAPSDK_080_COMPATIBILITYMODE
+        return hand.palmPosition();
+#else
+        return hand.stabilizedPalmPosition();
+#endif
+    }
+
+    OrbitManipulator::OrbitManipulator(const Mode& mode): osgGA::OrbitManipulator(),
+        mode_(mode),
+        leftHandID_(0),
+        rightHandID_(0),
+        lastLeftHand_(Leap::Hand()),
+        lastRightHand_(Leap::Hand()),
+        handsDistance_(0.0f),
+        currentAction_(LM_None),
+		modifier_(false),
+		modifierKey_(-1),
+		modifierMode_(MM_SIMPLE)
+    {
+		// Nothing to be done.
+    }
+
+    OrbitManipulator::~OrbitManipulator()
+    {
+		// Nothing to be done.
+    }
+
+    OrbitManipulator::OrbitManipulator(const OrbitManipulator& lm,
+        const osg::CopyOp& copyOp): osgGA::OrbitManipulator(lm, copyOp),
+        mode_(lm.mode_),
+        leftHandID_(0),
+        rightHandID_(0),
+        lastLeftHand_(Leap::Hand()),
+        lastRightHand_(Leap::Hand()),
+        handsDistance_(0.0f),
+        currentAction_(LM_None),
+		modifier_(lm.modifier_),
+		modifierKey_(lm.modifierKey_),
+		modifierMode_(lm.modifierMode_)
+    {
+
+    }
+
+    bool OrbitManipulator::handle( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us )
+    {
+		// Added to handle modifierKey_
+		if (modifierMode_ == MM_TOGGLE) {
+			if (ea.getEventType() == osgGA::GUIEventAdapter::KEYUP) {
+				if (modifierKey_ != -1 && ea.getKey() != -1 && ea.getKey() == modifierKey_) {
+					setModifierState(!modifier_);
+				}
+			}
+		} else if (modifierMode_ == MM_SIMPLE) {
+			if (ea.getEventType() == osgGA::GUIEventAdapter::KEYUP || ea.getEventType() == osgGA::GUIEventAdapter::KEYDOWN) {
+				if (modifierKey_ != -1 && ea.getKey() != -1 && ea.getKey() == modifierKey_) {
+					setModifierState(ea.getEventType() == osgGA::GUIEventAdapter::KEYDOWN);
+				}
+			}
+		}
+
+        if (ea.getEventType() == osgGA::GUIEventAdapter::USER) {
+			const osgLeap::Event* ev = dynamic_cast<const osgLeap::Event*>(&ea);
+			if (ev != NULL) {
+				const Leap::Frame& frame = ev->getFrame();
+
+				OSG_DEBUG_FP << "Frame id: " << frame.id()
+					<< ", timestamp: " << frame.timestamp()
+					<< ", hands: " << frame.hands().count()
+					<< ", fingers: " << frame.fingers().count()
+					<< ", tools: " << frame.tools().count()
+					<< ", gestures: " << frame.gestures().count() << std::endl;
+
+				if (!frame.hands().isEmpty()) {
+					Leap::Hand handRight;
+					Leap::Hand handLeft;
+					if (leftHandID_ == -1 || rightHandID_ == -1 || (leftHandID_ == rightHandID_ && frame.hands().count() > 1)) {
+						// Get the hands
+						handRight = frame.hands().rightmost();
+						handLeft = frame.hands().leftmost();
+						rightHandID_ = handRight.id();
+						leftHandID_ = handLeft.id();
+					} else {
+						handRight = frame.hand(rightHandID_);
+						handLeft = frame.hand(leftHandID_);
+						if (!handRight.isValid() || !handLeft.isValid()) {
+							// Get the hands
+							handRight = frame.hands().rightmost();
+							handLeft = frame.hands().leftmost();
+							rightHandID_ = handRight.id();
+							leftHandID_ = handLeft.id();
+						}
+					}
+					double reference_length = 100.0f;
+
+					if (mode_ == SingleHanded) {
+						if (frame.hands().count() > 0 && handRight.fingers().count() >= 3) {
+							if ((!modifier_ && !(currentAction_ & LM_Rotate))||(modifier_ && !(currentAction_ & LM_Pan))) {
+								lastRightHand_ = handRight;
+							}
+							if (!modifier_) {
+								currentAction_ = LM_Rotate | LM_Zoom;
+								Leap::Vector movement = (getPalmPosition(handRight)-getPalmPosition(lastRightHand_))/reference_length;
+								OSG_DEBUG<<"FIXED VERTICAL"<<std::endl;
+								rotateWithFixedVertical( movement.x, movement.y );
+								zoomModel(-movement.z);
+							} else {
+								currentAction_ = LM_Pan;
+								osg::Vec3 deltaPos = osg::Vec3(-(getPalmPosition(handRight).x-getPalmPosition(lastRightHand_).x), -(getPalmPosition(handRight).y-getPalmPosition(lastRightHand_).y), -(getPalmPosition(handRight).z-getPalmPosition(lastRightHand_).z));
+								if (deltaPos.x() != 0.0f || deltaPos.y() != 0.0f || deltaPos.z() != 0.0f) { 
+									// scale by model size to fit for very large and very small models
+									double factor = 2*us.asView()->getCamera()->getBound().radius();
+									// scale translation units. leap tracking: mm, OSG units: m
+									osg::Vec3 deltaTrans((deltaPos*factor/1000.0f));
+									panModel(deltaTrans.x(), deltaTrans.y());
+								}
+							}
+							us.requestRedraw();
+						} else {
+							currentAction_ = LM_None;
+						}
+					} else if (mode_ == Trackball) {
+						if (frame.hands().count() > 0 && handRight.fingers().count() >= 3) {
+							if (!(currentAction_ & LM_Rotate)) {
+								lastRightHand_ = handRight;
+							}
+							currentAction_ = LM_Rotate | LM_Zoom | LM_Pan;
+
+							if (currentAction_ & LM_Rotate) {
+								//Leap::FloatArray a(handRight.rotationMatrix(lastFrame_).toArray4x4());
+								//osg::Matrix mat_rot(a.m_array[0], a.m_array[1], a.m_array[2], a.m_array[3], a.m_array[4], a.m_array[5], a.m_array[6], a.m_array[7], a.m_array[8], 
+								//	a.m_array[9], a.m_array[10], a.m_array[11], a.m_array[12], a.m_array[13], a.m_array[14], a.m_array[15]);
+								//osg::Matrix rot(a.m_array[0], a.m_array[4], a.m_array[8], a.m_array[12], a.m_array[1], a.m_array[5], a.m_array[9], a.m_array[13], a.m_array[2], 
+								//	a.m_array[6], a.m_array[10], a.m_array[14], a.m_array[3], a.m_array[7], a.m_array[11], a.m_array[15]);
+								//setRotation(mat_rot.getRotate()*getRotation());
+
+								Leap::Vector lastRot(lastRightHand_.direction().yaw(), lastRightHand_.direction().pitch(), lastRightHand_.palmNormal().roll());
+								Leap::Vector curRot(handRight.direction().yaw(), handRight.direction().pitch(), handRight.palmNormal().roll());
+								Leap::Vector deltaRot = curRot - lastRot;
+								//if ((deltaRot.x != 0.0f) || (deltaRot.y != 0.0f) || (deltaRot.z != 0.0f)) { 
+								//	OSG_DEBUG<<"FIXED VERTICAL"<<std::endl;
+								//	rotateWithFixedVertical( -deltaRot.x, deltaRot.z );
+								//}
+
+								osg::Quat addRot(deltaRot.x, getRotation()*osg::Y_AXIS, -deltaRot.y, getRotation()*osg::X_AXIS, -deltaRot.z, getRotation()*osg::Z_AXIS);
+								setRotation(getRotation()*addRot); // does work only if no rotation is there..
+								us.requestRedraw();
+
+								//osg::Vec3 trans;
+								//osg::Quat rot;
+								//trans = getMatrix().getTrans();
+								//rot = getMatrix().getRotate();
+								//setByMatrix(/*osg::Matrix::rotate(addRot)**/osg::Matrix::rotate(rot)*osg::Matrix::translate(trans)*osg::Matrix::rotate(addRot));//flipping
+							}
+
+							if (currentAction_ & LM_Zoom) {
+								Leap::Vector movement = (getPalmPosition(handRight)-getPalmPosition(lastRightHand_))/reference_length;
+								zoomModel(-movement.z);
+								us.requestRedraw();
+							}
+
+							if (currentAction_ & LM_Pan) {
+								osg::Vec3 deltaPos = osg::Vec3(-(getPalmPosition(handRight).x-getPalmPosition(lastRightHand_).x), -(getPalmPosition(handRight).y-getPalmPosition(lastRightHand_).y), -(getPalmPosition(handRight).z-getPalmPosition(lastRightHand_).z));
+								if (deltaPos.x() != 0.0f || deltaPos.y() != 0.0f || deltaPos.z() != 0.0f) { 
+									// scale by model size to fit for very large and very small models
+									double factor = 2*us.asView()->getCamera()->getBound().radius();
+									// scale translation units. leap tracking: mm, OSG units: m
+									osg::Vec3 deltaTrans((deltaPos*factor/1000.0f));
+									panModel(deltaTrans.x(), deltaTrans.y());
+									us.requestRedraw();
+								}
+							}
+
+						} else {
+							currentAction_ = LM_None;
+						}
+					} else { //TwoHanded
+						if (frame.hands().count() == 1 && handRight.fingers().count() >= 3) {
+							if ((currentAction_ != LM_Pan)) {
+								lastRightHand_ = handRight;
+							}
+							currentAction_ = LM_Pan;
+						} else if (frame.hands().count() > 1 && handLeft.fingers().count() >= 3 && handRight.fingers().count() >=3) {
+							currentAction_ = LM_Rotate;
+						} else if (frame.hands().count() > 1 && 
+							((handLeft.fingers().count() >= 3 && handRight.fingers().count() <= 1) || (handLeft.fingers().count() <= 1 && handRight.fingers().count() >= 3 ) ))
+						{
+							if ((currentAction_ != LM_Zoom)) {
+								handsDistance_ = (getPalmPosition(handLeft) - getPalmPosition(handRight)).magnitude();
+							}
+							currentAction_ = LM_Zoom;
+						} else {
+							currentAction_ = LM_None;
+						}
+
+						OSG_DEBUG<<"MODE: ";
+						if (currentAction_ & LM_Rotate) OSG_DEBUG<<"ROTATE ";
+						if (currentAction_ & LM_Zoom) OSG_DEBUG<<"ZOOM ";
+						if (currentAction_ & LM_Pan) OSG_DEBUG<<"PAN ";
+						if (currentAction_ == 0) OSG_DEBUG<<"NONE";
+						OSG_DEBUG<<std::endl;
+
+						osg::Vec3 trans;
+						osg::Quat rot;
+						osg::Vec3 scale;
+						osg::Quat so;
+
+						rot = getMatrix().getRotate();
+						trans = getMatrix().getTrans();
+
+						// Calculate delta position (movement)
+						osg::Vec3 deltaPos = osg::Vec3(-(getPalmPosition(handRight).x-getPalmPosition(lastRightHand_).x), -(getPalmPosition(handRight).y-getPalmPosition(lastRightHand_).y), -(getPalmPosition(handRight).z-getPalmPosition(lastRightHand_).z));
+						if (currentAction_ & LM_Pan) {
+							if (deltaPos.x() != 0.0f || deltaPos.y() != 0.0f || deltaPos.z() != 0.0f) { 
+								// scale by model size to fit for very large and very small models
+								double factor = 2*us.asView()->getCamera()->getBound().radius();
+								// scale translation units. leap tracking: mm, OSG units: m
+								osg::Vec3 deltaTrans((deltaPos*factor/1000.0f));
+								trans += rot*deltaTrans;
+								panModel(deltaTrans.x(), deltaTrans.y());
+								us.requestRedraw();
+							}
+						}
+
+						double distance = (getPalmPosition(handLeft) - getPalmPosition(handRight)).magnitude();
+						if (handsDistance_ != 0.0f) {
+							if (currentAction_ & LM_Zoom) {
+								double factor = (handsDistance_-distance)/(reference_length);
+								if (factor > 1.0f || factor < -1.0f) { factor = 1.0f; }
+								zoomModel(factor);
+								us.requestRedraw();
+							}
+							if (currentAction_ & LM_Rotate) {
+#if 0
+								Leap::Vector movement = osg::PI_2*(handRight.stabilizedPalmPosition()-lastRightHand_.stabilizedPalmPosition())/reference_length;
+								osg::Quat addRotX(-movement.x, osg::Y_AXIS);
+								osg::Quat addRotY(movement.y, osg::X_AXIS);
+								osg::Quat addRotZ;//(movement.z, osg::Z_AXIS);//movement very strange
+								manipulator_->setRotation(addRotX*addRotY*addRotZ*rot);
+#else
+								// At the moment, Fixed VerticalAxis is the only mode supported
+								// because rotateTrackball is not working correctly, yet.
+								if( true /*manipulator_->getVerticalAxisFixed()*/ ) {
+									Leap::Vector movement = (getPalmPosition(handRight)-getPalmPosition(lastRightHand_))/reference_length;
+									OSG_DEBUG<<"FIXED VERTICAL"<<std::endl;
+									rotateWithFixedVertical( movement.x, movement.y );
+									us.requestRedraw();
+								} else {
+									Leap::Vector lastPosNorm = getPalmPosition(lastRightHand_)/reference_length;
+									Leap::Vector curPosNorm = getPalmPosition(handRight)/reference_length;
+									OSG_DEBUG<<"FLOATING VERTICAL"<<std::endl;
+									//rotateTrackball( lastPosNorm.x, lastPosNorm.y,
+									//	curPosNorm.x, curPosNorm.y,
+									//	getThrowScale( deltaTimeSinceLastFrame ) );
+									us.requestRedraw();
+								}
+#endif
+							}
+						}
+
+						handsDistance_ = distance;
+
+					}
+
+					lastLeftHand_ = handLeft;
+					lastRightHand_ = handRight;
+
+				} else {
+					currentAction_ = LM_None;
+				}
+
+			}
+		}
+
+        return osgGA::OrbitManipulator::handle(ea, us);
+    }
+
+}
---
+++ tags/osgLeap-0.5.0/src/osgLeap/PointerGraphicsUpdateCallback.cpp
@@ -0,0 +1,146 @@
+/*
+* Library osgLeap
+* Copyright (C) 2013 Johannes Kroeger/vtxtech. All rights reserved.
+*
+* This file is licensed under the GNU Lesser General Public License 3 (LGPLv3),
+* but distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+*/
+
+#include <osgLeap/PointerGraphicsUpdateCallback>
+
+//-- OSG: osg --//
+#include <osg/io_utils>
+#include <osg/Material>
+#include <osg/PositionAttitudeTransform>
+#include <osg/ShapeDrawable>
+#include <osg/ValueObject>
+
+namespace osgLeap {
+
+    osg::ref_ptr<osg::Node> PointerGraphicsUpdateCallback::createPointerGeode() {
+        osg::ref_ptr<osg::Geode> sphere = new osg::Geode();
+        float radius = 10.0f;
+        osg::TessellationHints* hints = new osg::TessellationHints();
+        hints->setDetailRatio(0.5f);
+        osg::ref_ptr<osg::ShapeDrawable> sd = new osg::ShapeDrawable(new osg::Sphere(osg::Vec3(0.0f,0.0f,0.0f), radius), hints);
+        sd->setColor(getColor());
+        sphere->addDrawable(sd);
+        return sphere;
+    }
+
+    osg::Vec4 PointerGraphicsUpdateCallback::getColor() {
+        osg::Vec4 result;
+
+        switch (colorIndex_) {
+        case 0:
+            result = osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f);
+            break;
+        case 1:
+            result = osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f);
+            break;
+        case 2:
+            result = osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f);
+            break;
+        case 3:
+            result = osg::Vec4(1.0f, 1.0f, 0.0f, 1.0f);
+            break;
+        case 4:
+            result = osg::Vec4(0.0f, 1.0f, 1.0f, 1.0f);
+            break;
+        case 5:
+            result = osg::Vec4(1.0f, 0.0f, 1.0f, 1.0f);
+            break;
+        case 6:
+            result = osg::Vec4(1.0f, 0.5f, 0.0f, 1.0f);
+            break;
+        case 7:
+            result = osg::Vec4(0.5f, 0.1f, 0.0f, 1.0f);
+            break;
+        case 8:
+            result = osg::Vec4(1.0f, 0.5f, 0.5f, 1.0f);
+            break;
+        case 9:
+            result = osg::Vec4(1.0f, 1.0f, 0.5f, 1.0f);
+            break;
+        default:
+            result = osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f);
+            break;
+        }
+
+        colorIndex_++;
+        if (colorIndex_ > 9) { colorIndex_ = 0; }
+
+        return result;
+    }
+
+    void osgLeap::PointerGraphicsUpdateCallback::operator()(osg::Node* node, osg::NodeVisitor* nv)
+    {
+        // Grab data from Leap Motion
+        intersectionController_->update();
+
+        // Now update our Geode to display the pointers
+        osg::ref_ptr<osg::Group> group = dynamic_cast<osg::Group*>(node);
+        if (group.valid()) {
+            group->setDataVariance(osg::Object::DYNAMIC);
+
+            osgLeap::PointerMap pointers = intersectionController_->getPointers();
+
+            PatMap transforms;
+            // Remove any pointers not visible anymore
+            for (int n = group->getNumChildren()-1; n >= 0; --n) {
+                int pid = -1;
+                bool remove = true;
+                if (group->getChild(n)->getUserValue<int>("PointableID", pid)) {
+                    for (osgLeap::PointerMap::iterator itr = pointers.begin(); itr != pointers.end(); ++itr) {
+                        // Still visible in current frame
+                        if (itr->first == pid) remove = false;
+                    }
+                }
+                if (remove) {
+                    group->removeChild(n);
+                } else {
+                    transforms.insert(PatPair(pid, dynamic_cast<osg::PositionAttitudeTransform*>(group->getChild(n))));
+                }
+            }
+
+            // Add more pointers if required or update if they have moved
+            for (osgLeap::PointerMap::iterator itr = pointers.begin(); itr != pointers.end(); ++itr) {
+                osg::ref_ptr<osgLeap::Pointer> p = itr->second;
+
+                PatMap::iterator patitr = transforms.find(itr->first);
+                osg::ref_ptr<osg::PositionAttitudeTransform> pat = NULL;
+                if (patitr != transforms.end()) {
+                    pat = patitr->second;
+                    if (referenceTime_ != 0) {
+                        osg::Geode* geode = dynamic_cast<osg::Geode*>(pat->getChild(0));
+                        if (geode) {
+                            osg::ShapeDrawable* sd = dynamic_cast<osg::ShapeDrawable*>(geode->getDrawable(0));
+                            float f = 1-p->clickTimeProgress(referenceTime_);
+                            osg::Vec4 color(sd->getColor().x(), sd->getColor().y(), sd->getColor().z(), f);
+                            sd->setColor(color);
+                            sd->getOrCreateStateSet()->setMode(GL_BLEND, osg::StateAttribute::ON);
+                            sd->getOrCreateStateSet()->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
+                        }
+                    }
+                } else {
+                    // Add new pointer
+                    pat = new osg::PositionAttitudeTransform();
+                    pat->setUserValue<int>("PointableID", itr->first);
+                    pat->addChild(createPointerGeode());
+                    // Add to scene graph
+                    group->addChild(pat);
+                    // Add to local reference map
+                    transforms.insert(PatPair(itr->first, pat));
+                }
+
+                osg::Vec3 vec = osg::Vec3(p->getPosition().x(), p->getPosition().y(), 0.0f);
+                pat->setPosition(vec);
+            }
+        }
+
+        traverse(node, nv);
+    }
+
+} // namespace osgLeap
